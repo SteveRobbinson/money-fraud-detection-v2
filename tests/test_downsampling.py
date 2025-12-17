ab300d7982b864e5faebea74ea92b09dcda52b69
Luki <lukasjagusz6@gmail.com>
2025-12-17 14:51:22 +0100

    fix pytest fixture dependencies

--- tests/test_downsampling.py
+++ tests/test_downsampling.py
@@ -1,12 +1,12 @@
 from forest.downsample_majority import downsample_majority 
 from forest import config
 
-def test_downsampling(df_preprocessed):
+def test_downsampling(preprocessed_df):
     
-    df_downsampled = downsample_majority(df_preprocessed, config.num_samples_test)
+    df_downsampled = downsample_majority(preprocessed_df, config.num_samples_test)
     
-    assert len(df_downsampled) < len(df_preprocessed)
+    assert len(df_downsampled) < len(preprocessed_df)
 
-    before = (df_preproccessed['is_fraud'] == True).sum()
+    before = (preprocessed_df['is_fraud'] == True).sum()
     after = (df_downsampled['is_fraud'] == True).sum()
     assert before == after
--- tests/test_splitting.py
+++ tests/test_splitting.py
@@ -1,15 +1,15 @@
 from forest.data_preprocessing import split_features_target
 import numpy as np
 
-def test_splitting(df_preprocessed):
-    X, y = split_features_target(df_preprocessed, 'is_fraud')
+def test_splitting(preprocessed_df):
+    X, y = split_features_target(preprocessed_df, 'is_fraud')
 
     assert 'is_fraud' not in X.columns
 
     unique_vals = np.unique(y)
     assert np.isin(unique_vals, [0, 1, True, False]).all()
 
-    assert len(df_preprocessed.drop('is_fraud').columns) == len(X.columns)
+    assert len(preprocessed_df.drop('is_fraud').columns) == len(X.columns)
 
-    assert y.reset_index(drop=True).equals(df_preprocessed["is_fraud"].reset_index(drop=True))
+    assert y.reset_index(drop=True).equals(preprocessed_df["is_fraud"].reset_index(drop=True))
         
