f18d2dff2eeb6d14e1648bd202fd11d32fd9540b
Luki <lukasjagusz6@gmail.com>
2025-12-17 11:26:46 +0100

    make sample size configurable via num_samples argument

--- src/forest/downsample_majority.py
+++ src/forest/downsample_majority.py
@@ -1,10 +1,14 @@
 import pandas as pd
 
-def downsample_majority(df: pd.DataFrame) -> pd.DataFrame:
+def downsample_majority(
+        df: pd.DataFrame,
+        num_samples: int
+        ) -> pd.DataFrame:
+
     df_not_fraud = df[df['is_fraud'] == False]
     df_fraud = df[df['is_fraud'] == True]
 
-    df_not_fraud = df_not_fraud.sample(1000000, random_state=43)
+    df_not_fraud = df_not_fraud.sample(num_samples, random_state=43)
     df_downsampled = pd.concat([df_not_fraud, df_fraud])
 
     return df_downsampled.sample(frac=1).reset_index(drop=True)
