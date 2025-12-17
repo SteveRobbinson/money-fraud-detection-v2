e3d0cfd4ccc82272ceddffab2b6307ea60f8fbe5
Luki <lukasjagusz6@gmail.com>
2025-12-17 13:35:21 +0100

    add shared pytest fixtures for ML pipeline

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,16 +1,35 @@
-import pandas as pd
+from forest.dataset_split import create_sets
+from forest.downsample_majority import downsample_majority
 from forest import config
 import pytest
-from forest.data_preprocessing import preprocess
+from forest.data_preprocessing import preprocess, split_features_target
 
 @pytest.fixture
 def preprocessed_df():
-    df = preprocess(
+    preprocessed_df = preprocess(
     config.test_path,
     config.get_features,
     config.dummies,
     config.drop_list
     )
     
-    return df
+    return preprocessed_df
+
+@pytest.fixture
+def downsampled_df(preprocessed_df):
+    df_downsampled = downsample_majority(preprocessed_df, config.num_samples_test)
+
+    return df_downsampled
+
+@pytest.fixture
+def X_y(df_downsampled):
+    X, y = split_features_target(df_downsampled, 'is_fraud')
+
+    return X, y
+
+@pytest.fixture
+def sets(X, y):
+    X_train, X_test, y_train, y_test = create_sets(X, y, 0.1)
+
+    return X_train, X_test, y_train, y_test
 
