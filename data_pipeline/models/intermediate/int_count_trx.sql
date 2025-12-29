d0d70d80b6a96955accb5a44bddf8c0ed2a06a91
Luki <lukasjagusz6@gmail.com>
2025-12-29 19:44:21 +0100

    shorten size of time_windows, rename file

--- data_pipeline/models/intermediate/int_count_trx.sql
+++ data_pipeline/models/intermediate/int_count_trx.sql
@@ -1,17 +1,20 @@
 {% set dimensions_full = ['sender_account', 'receiver_account', 'device_hash', 'ip_address'] %}
+{% set time_windows = ['1 hour', '24 hour', '30 day'] %}
 with staging_data as (
   select * from {{ ref('stg_transactions') }}
 ),
 
+{# dla sender, receiver, device, ip licze liczbe transakcji wykonanych w danym oknie czasowym #}
+
 windows_computed as (
   select
-    * 
+    transaction_id
     {% for dim in dimensions_full %}
-      {% for i in range (var('time_frames')|length) %}
+      {% for i in range(time_windows|length) %}
         , count(*) over(
           partition by {{ dim }}
           order by timestamp
-          range between interval '{{ var('time_frames')[i] }}' preceding and current row
+          range between interval '{{ time_windows[i] }}' preceding and current row
         ) as {{ dim.split('_')[0] }}_num_trx_{{ var('time_suffix')[i] }}
       {% endfor %}
     {% endfor %}
